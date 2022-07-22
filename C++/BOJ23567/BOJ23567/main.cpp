a80c3dbc40575cac8b2113ea1355e61a7206e4c6
Jin-won Choi(Kelly Chui) <optioneldar@gmail.com>
2022-07-22 17:57:29 +0900

    [BOJ]23567-CPP

---
+++ C++/BOJ23567/BOJ23567/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BOJ23567
+//
+//  Created by Kelly Chui on 2022/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
