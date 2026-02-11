177f785df56e107b495898f5532c603f5f2fd10e
kelly-chui <dev.kellychui@gmail.com>
2026-02-11 14:38:50 +0900

    delete TS directory

--- TypeScript/BOJ/BOJ25305.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from "fs";
-
-const input = fs.readFileSync(0).toString().trim().split('\n');
-const [N, K] = input[0].split(' ').map(Number);
-const scores: number[] = input[1].split(' ').map(Number);
-scores.sort((a, b) => b - a);
-console.log(scores[K - 1]);
